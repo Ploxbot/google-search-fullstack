0a7cf039323dd6c9b99e437bba63c52fe9c0ea5d
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-01-25 16:32:35 +0700

    fixed results controller route

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./controllers/search')
-// app.use('/results', require('./controller')
+app.use('/results', require('./controllers/results')
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {

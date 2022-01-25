8e65da59871b1475bb59e5eef7beb41499d27e28
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-01-25 16:21:40 +0700

    moved routes to controller

--- controllers/results.js
+++ controllers/results.js
@@ -1,5 +1,10 @@
 // Import Packages
-
+const express = require('express')
+const router = express.Router()
 // Create POST controller
+router.get('/', (req, res) => {
+	res.send('This is the Resultspage')
+})
 
 // Export module
+module.exports = router

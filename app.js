4ad860124ba028f466524bddaf366baceb77c4aa
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-01-25 16:31:07 +0700

    fixed controller routes

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 // )
 
 // Routes
-app.use('/', require('controller')
-app.use('/results', require('./controller')
+app.use('/', require('./controllers/search')
+// app.use('/results', require('./controller')
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
--- controllers/results.js
+++ controllers/results.js
@@ -1,6 +1,7 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+
 // Create POST controller
 router.get('/', (req, res) => {
 	res.send('This is the Resultspage')

8ebab5dcd3da711847dd7a6e65fb45e5c84b63ee
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-01-27 10:13:34 +0700

    env

--- controllers/search.js
+++ controllers/search.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-	res.send('This is the Searchpage')
+router.post('/', (req, res) => {
+	res.render('/results')
 })
 
 // Views
